db86389cb3a8debfea14f867b30364b5fed58dc9
Cynthia Nwakaeme <nwakaemecynthia@gmail.com>
2023-08-12 14:08:50 +0100

    Adjusted design on mobile screen amd improved form validation

--- src/App.js
+++ src/App.js
@@ -1,82 +1,24 @@
+import {Fragment} from 'react';
+import {Toaster} from "react-hot-toast";
 import './App.css';
-import LandingPage from './landingPage';
-import { useState, Fragment } from 'react';
-import axios from 'axios';
-import {Toaster, toast} from "react-hot-toast";
+import LandingPage from './LandingPage';
 
 function App() {
-  const [loading, setLoading] = useState(false);
-    const [errors, setErrors] = useState('')
-    const [formData, setformData] = useState({name: '', email: '' })
-
-    const handleSubmit = (event) =>{
-        event.preventDefault();
-        const errors = validate();
-        setErrors(errors)
-        console.log(formData)
-        postData()
-
-        if( Object.keys(errors).length === 0){
-            alert("Done")
-        }
-    }
-
-    const validate = ()=>{
-        const error = {}
-
-        if(!formData.name){
-            error.name= "Name is Required"
-        }else{
-            error.name=""
-        }
-
-        if(!formData.email){
-            error.email = "E-mail is required"
-        }else if (!/^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/.test(formData.email)){
-            error.email = "Must be a valid e-mail address"
-        }else{
-          error.email = ""
-        }
-
-        return error;
-    }
 
 
-    const postData = async()=>{
-      setLoading(true)
-      const {data: responseData} = await axios.post(`https://backoffice-staging-2ric45myja-uc.a.run.app/messaging-service/subscribe`, 
-      formData, 
-      {headers: {"Content-Type": "application/json"}})
-
-      if(responseData.status !== "SUCCESS") {
-        toast.error(responseData.message)
-      }else {
-        toast.success(responseData.message);
-        setformData({name: '', email: '' })
-      }
-      setLoading(false)
-    }
-
-    const handleChange = (e)=>{
-      const name = e.target.name
-      const value = e.target.value
-      
-
-      setformData({...formData, [name]:value })
-    }
   return (
     <Fragment>
-      <Toaster containerStyle={{zIndex: '99999999'}}/>
-    <div className="container">
-      <LandingPage submit={handleSubmit}  errors={errors} data={handleChange} diasble={loading}/>
-    </div>
+      <Toaster containerStyle={{zIndex: '99999999'}} position="top-right" reverseOrder={false}/>
+      <div className="container">
+        <LandingPage/>
+      </div>
 
-    <div className='footer'>
-      <p> 
-        Copyright © {new Date().getFullYear()} <a href='https://www.truq.it/'>Siju by truQ</a>. All right reserved
-      </p>
-    </div>
-    
+      <div className='footer'>
+        <p>
+          Copyright © {new Date().getFullYear()} <a href='https://www.truq.it/'>Siju by truQ</a>. All right reserved
+        </p>
+      </div>
+
     </Fragment>
   );
 }
---
+++ src/LandingPage.js
@@ -0,0 +1,130 @@
+import './LandingPage.css';
+import {useState} from "react";
+import {toast} from "react-hot-toast";
+import axios from "axios";
+import webinar from './assets/Webinar-Banner.png';
+import loadIcon from './assets/loading.svg';
+
+
+const LandingPage = () => {
+  const [loading, setLoading] = useState(false);
+  const [formData, setFormData] = useState({name: '', email: ''});
+  const [errors, setErrors] = useState({name: true, email: true});
+  const [errorMessages, setErrorMessages] = useState({name: "", email: ""});
+  const isInvalid = !formData.name || !formData.email || errors.name || errors.email;
+
+  const handleChange = e => {
+    const name = e.target.name;
+    const value = name === "email" ? e.target.value.toLowerCase() : e.target.value;
+    setFormData({...formData, [name]: value});
+    validate(e);
+  }
+
+  const validate = event => {
+    const error = {...errors};
+    const errorMgs = {...errorMessages};
+
+    if (event.target.name === "name") {
+      if (!formData.name || formData.name.length < 2) {
+        error.name = true;
+        errorMgs.name = "Full name is Required";
+      }
+      else {
+        error.name = false;
+        errorMgs.name = null;
+      }
+    } else {
+      if (!formData.email) {
+        error.email = true;
+        errorMgs.email = "Email is required";
+      }
+      else if (!/^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{1,}$/.test(formData.email)) {
+        error.email = true;
+        errorMgs.email = "Invalid email address";
+      }
+      else {
+        error.email = false;
+        errorMgs.email = null;
+      }
+    }
+
+    setErrors(error);
+    setErrorMessages(errorMgs);
+  }
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    setLoading(true);
+
+    axios.post(
+      `https://backoffice-staging-2ric45myja-uc.a.run.app/messaging-service/subscribe`, formData,
+      {headers: {"Content-Type": "application/json"}}
+    ).then((results) => {
+      const {data: responseData} = results;
+      toast.success(responseData.message);
+      setFormData({name: '', email: ''});
+      setLoading(false);
+    }).catch(err => {
+      const error = (err && err.response && err.response.data && err.response.data.message) ? err.response.data.message : "Cannot handle this request at the moment orSubscriber email already exists.";
+      toast.error(error);
+      setLoading(false);
+    });
+  }
+
+
+  return (
+    <div className='form-container'>
+      <section>
+        <div className='imgcontainer'>
+          <img src={webinar} className="image" alt="webinar-img" loading="eager"/>
+        </div>
+        <h1>Step into the future with us!</h1>
+        <h2>We are optimising the Logistics Operations in Africa.</h2>
+        <h2>Join the Conversation!</h2>
+        <div className='det'>
+          <p>Date: Wednesday, 30th August 2023 <br/>
+            Time: 2 PM<br/>
+            Venue: Google Meet</p>
+        </div>
+        <p className='para2'>Discover the Future of Third-Party Logistics at Optimise 1.0</p>
+
+        <p className='para3'>What to expect?</p>
+      </section>
+
+      <section className='section2'>
+        <ul>
+          <li>Unravel the Latest Industry Trends that will revolutionize the Logistics space.</li>
+          <li>Discover how digitization and automation increases business profit margin.</li>
+          <li className='lastitem'>Gain Insights from Top Guest Speakers.</li>
+        </ul>
+
+        <p className='para4'>Limited spots available! Register Now!</p>
+      </section>
+
+
+      <form onSubmit={handleSubmit}>
+        <div className='formgroup'>
+          <label htmlFor='name'>Name:</label><br/>
+          <input type='text' name='name' onChange={handleChange} value={formData.name}/>
+          {(errors.name && errorMessages.name) && <div className='error'>{errorMessages.name}</div>}
+        </div>
+        <br/>
+        <div className='formgroup'>
+          <label htmlFor='email'>Email:</label><br/>
+          <input type='text' name='email' onChange={handleChange} value={formData.email}/>
+          {(errors.email && errorMessages.email) && <div className='error'>{errorMessages.email}</div>}
+        </div>
+        <br/>
+        <div className='formgroup'>
+          <button className='submit' id='btn' type='submit' disabled={isInvalid}>
+            Save My Spot
+            {loading && <img src={loadIcon} width={18} height={18} alt="spinner"/>}
+          </button>
+        </div>
+      </form>
+    </div>
+
+  )
+}
+
+export default LandingPage;
